8c3a30018c232d5e5a8703b95e201777cc0dc18c
ThisGuy-ks <67611311+ThisGuy-ks@users.noreply.github.com>
2020-07-11 17:45:58 -0700

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class RockPaperScissors{
+
+	public static void main(String[] args){
+		
+		Random computerDecision = new Random();
+		int cdecision = computerDecision.nextInt(3);
+		//0 = rock
+		//1 = paper
+		//2 = scissors
+
+
+		Scanner playerDecision = new Scanner(System.in);
+		System.out.println("Pick between 'rock', 'paper', or 'scissors'.");
+		String pdecision = playerDecision.nextLine();
+
+		if (!pdecision.equals("rock") && !pdecision.equals("paper") && !pdecision.equals("scissors")) {
+		
+		} else if (cdecision == 0) {
+			System.out.println("Computer chooses rock");
+		} else if (cdecision == 1) {
+			System.out.println("Computer chooses paper");
+		} else if (cdecision == 2) {
+			System.out.println("Computer chooses scissors");
+		}
+
+
+		if (!pdecision.equals("rock") && !pdecision.equals("paper") && !pdecision.equals("scissors")) {
+			System.out.println("Invalid input.");
+		} else if (pdecision.equals("rock") && cdecision == 1) {
+			System.out.println("Computer wins!");
+		} else if (pdecision.equals("rock") && cdecision == 2) {
+			System.out.println("Player wins!");
+		} else if (pdecision.equals("paper") && cdecision == 2) {
+			System.out.println("Computer wins!");
+		} else if (pdecision.equals("paper") && cdecision == 0) {
+			System.out.println("Player wins!");
+		} else if (pdecision.equals("scissors") && cdecision == 0) {
+			System.out.println("Computer wins!");
+		} else if (pdecision.equals("scissors") && cdecision == 1) {
+			System.out.println("Player wins!");
+		} else {
+			System.out.println("Tie!");
+		}
+
+	}
+}
